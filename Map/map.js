447b3e2a068b4332d679a8abdff75600ee6bbd0a
Brevan Bender <bcaybender@gmail.com>
2021-10-27 11:40:08 -0700

    debugging and finsihing up maybe

--- Map/map.js
+++ Map/map.js
@@ -20,12 +20,17 @@ function displayLink(quest){
     const a = document.createElement('a');
     a.href = `../quest/?id=${quest.id}`;
     a.textContent = quest.title;
+    a.classList.add('quest');
     questLink.appendChild(a);
+    a.style.top = quest.map.top;
+    a.style.left = quest.map.left;
 }
 
 function displaySpan(quest){
     const span = document.createElement('span');
     span.textContent = quest.title;
-
+    span.classList.add('quest');
     questLink.appendChild(span);
+    span.style.left = quest.map.left;
+    span.style.top = quest.map.left;
 }
